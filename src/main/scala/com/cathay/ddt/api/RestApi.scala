cb9e10764307600c3cd294db050a5603f25ca3b4
zerostyle0327 <zerostyle0327@gmail.com>
2018-04-12 09:26:56 +0800

    [bugfix] typesafe config add in api server

--- src/main/scala/com/cathay/ddt/api/RestApi.scala
+++ src/main/scala/com/cathay/ddt/api/RestApi.scala
@@ -7,13 +7,13 @@ import com.cathay.ddt.utils.EnvLoader
 
 object RestApi extends App with ApiRoute with EnvLoader {
 
+  val config = getConfig("api")
   override implicit val system = ActorSystem("mongo-rest-api", config)
 
   override implicit val materializer = ActorMaterializer()
 
   override implicit val ec = system.dispatcher
 
-  val config = getConfig("api")
   val bindingFuture = Http().bindAndHandle(route, config.getString("api.host"), config.getInt("api.port"))
 
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
